a225384b98ce002911b2439d9f43f46b2390ac37
Jim Bothén <jim.bothen@gmail.com>
2021-12-01 17:40:16 +0100

    Layout, basic styling, basic functionality

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 window.addEventListener("load", main)
-function main()
+function main() {
+    eventListeners()
+}
+
+function eventListeners() {
+    const button1 = document.querySelector("#experienceButton")
+    button1.addEventListener("click", expandDiv1)
+    const button2 = document.querySelector("#aboutMeButton")
+    button2.addEventListener("click", expandDiv2)
+    const button3 = document.querySelector("portfolioButton")
+    button3.addEventListener("click", expandDiv3)
+}
+
+function expandDiv1() {
+    const div1 = document.querySelector("#experience")
+    const div2 = document.querySelector("#aboutMe")
+    const div3 = document.querySelector("#portfolio")
+    div1.classList.replace("hidden", "flex")
+    div2.classList.replace("flex", "hidden")
+    div3.classList.replace("flex", "hidden")
+}
+
+function expandDiv2() {
+    const div1 = document.querySelector("#experience")
+    const div2 = document.querySelector("#aboutMe")
+    const div3 = document.querySelector("#portfolio")
+    div2.classList.replace("hidden", "flex")
+    div1.classList.replace("flex", "hidden")
+    div3.classList.replace("flex", "hidden")
+}
+
+function expandDiv3() {
+    const div1 = document.querySelector("#experience")
+    const div2 = document.querySelector("#aboutMe")
+    const div3 = document.querySelector("#portfolio")
+    div3.classList.replace("hidden", "flex")
+    div1.classList.replace("flex", "hidden")
+    div2.classList.replace("flex", "hidden")
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      'primary': '#CE5633',
+      'secondary': '#EEE5E9',
+      'terciary': '#81B29A',
+      'footer-color': '#71A2B6',
+      'font-color-black': '#444140',
+      'font-color-beige': '#F1D9BF',
+    },
     extend: {},
   },
   variants: {
